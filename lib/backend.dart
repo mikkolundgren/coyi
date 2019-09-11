fe1618361b6e626ba7f4756ead2e7aaf3342950b
mikko <mikko.lundgren@gmail.com>
2019-09-11 20:59:28 +0300

    started with the backend

--- lib/backend.dart
+++ lib/backend.dart
@@ -1,1 +1,41 @@
+import 'secrets/secrets.dart';
+import 'dart:convert';
+import 'constants.dart';
+import 'package:http/http.dart' as http;
 
+Map<String, String> _headers = {
+  'X-RapidAPI-Host': HOST,
+  'X-RapidAPI-Key': API_KEY
+};
+
+Future<List<Stats>> getStatistics() async {
+  var client = http.Client();
+
+  var result = List<Stats>();
+  try {
+    var whResponse = await client.get(
+        BASE_URL + '/statistics/' + PREMIER_LEAGUE + '/' + WEST_HAM,
+        headers: _headers);
+    Stats wh = Stats.fromJson(jsonDecode(whResponse.body));
+    result.add(wh);
+    var brResponse = await client.get(
+        BASE_URL + '/statistics/' + PREMIER_LEAGUE + '/' + BRIGHTON,
+        headers: _headers);
+    Stats br = Stats.fromJson(jsonDecode(brResponse.body));
+    result.add(br);
+  } finally {
+    client.close();
+  }
+  return result;
+}
+
+class Stats {
+  final int wins;
+  final int loses;
+  final int draws;
+  Stats(this.wins, this.loses, this.draws);
+  Stats.fromJson(Map<String, dynamic> data)
+      : wins = int.parse(data['api']['statistics']['matchs']['wins']),
+        loses = int.parse(data['api']['statistics']['matchs']['loses']),
+        draws = int.parse(data['api']['statistics']['matchs']['draws']);
+}
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
 const PREMIER_LEAGUE = '524';
 const WEST_HAM = '48';
 const BRIGHTON = '51';
+
+const BASE_URL = 'https://api-football-v1.p.rapidapi.com/v2';
